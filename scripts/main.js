6c22869d0682ee28e835bfff62049541c6c10c34
Edgar Miranda <edgar.j.miranda@me.com>
2018-10-08 13:28:54 -0500

    Add function to populate display output when a number key is pressed

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+const output = document.querySelector(`.output`);
+const numbers = document.querySelectorAll(`.number`);
+
+for (let number of numbers) {
+  number.addEventListener(`click`, () => {
+    output.textContent += number.textContent;
+  });
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case `+`:
+      return add(num1, num2);
+    case `−`:
+      return substract(num1, num2);
+    case `×`:
+      return multiply(num1, num2);
+    case `÷`:
+      return divide(num1, num2);
+  }
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -15,17 +37,4 @@ function divide(num1, num2) {
     return num1 / num2;  
   }
   return `ERROR`;
-}
-
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case `+`:
-      return add(num1, num2);
-    case `-`:
-      return substract(num1, num2);
-    case `*`:
-      return multiply(num1, num2);
-    case `/`:
-      return divide(num1, num2);
-  }
 }
