fe447931cab2d440dc4e93bf44b60d19b3b6a7a1
Edgar Miranda <edgar.j.miranda@me.com>
2018-10-09 15:19:51 -0500

    Add format to ouput with comma for thousands

--- scripts/main.js
+++ scripts/main.js
@@ -3,45 +3,48 @@ const clear = document.querySelector(`.clear`);
 const sign = document.querySelector(`.sign`);
 const perc = document.querySelector(`.perc`);
 const numbers = document.querySelectorAll(`.number`);
+const dot = document.querySelector(`.dot`);
 const operations = document.querySelectorAll(`.operation`);
 const equal = document.querySelector(`.equal`);
 
-let scrValue = `0`;
+let value = `0`;
 let num1 = 0;
 let num2 = 0;
 let operator = ``;
 let isAfterOpSelect = false;
 
 clear.addEventListener(`click`, () => {
-  scrValue = `0`;
-  showOutput(scrValue);
+  value = `0`;
+  showOutput(value);
 });
 
 sign.addEventListener(`click`, () => {
-  if (scrValue.startsWith(`-`)) {
-    scrValue = scrValue.substring(1);
+  if (value.startsWith(`-`)) {
+    value = value.substring(1);
   } else {
-    scrValue = `-${scrValue}`;
+    value = `-${value}`;
   }
-  showOutput(scrValue);
+  showOutput(value);
 });
 
 perc.addEventListener(`click`, () => {
-  if (scrValue !== `0`) {
-    scrValue = +scrValue / 100;
+  if (value !== `0`) {
+    value = (+value / 100).toString();
   }
-  showOutput(scrValue);
+  showOutput(value);
 });
 
 for (let number of numbers) {
   number.addEventListener(`click`, () => {
     deselectOperations();
-    if ((scrValue === `0` && number.textContent !== `.`) || isAfterOpSelect) {
-      scrValue = ``;
+    if ((/-*0$/.test(value) && number.textContent !== `.`) 
+        || isAfterOpSelect) {
+      value = /-/.test(value) ? `-` : ``;
       isAfterOpSelect = false;
     }
-    scrValue += number.textContent;
-    showOutput(scrValue);
+    value += number.textContent;
+    checkDot(value);
+    showOutput(value);
   });
 }
 
@@ -50,28 +53,63 @@ for (let operation of operations) {
     deselectOperations();
     operation.classList.add(`selected`);
     operator = event.target.textContent;
-    num1 = +scrValue;
+    num1 = +value;
+    value = `0`;
     isAfterOpSelect = true;
   });
 }
 
 equal.addEventListener(`click`, () => {
   deselectOperations();
-  num2 = +scrValue;
-  scrValue = operate(operator, num1, num2).toString();
-  showOutput(scrValue);
+  num2 = +value;
+  value = operate(operator, num1, num2).toString();
+  showOutput(value);
   console.log(num1, operator, num2);
 });
-
-function showOutput(string) {
-  output.textContent = string;
-}
 
 function deselectOperations() {
   for (let operation of operations) {
     operation.classList.remove(`selected`);
   }
 }
+
+function checkDot(string) {
+  if (/\./.test(string)) {
+    dot.disabled = true;
+  } else {
+    dot.disabled = false;
+  }
+}
+
+function showOutput(string) {
+  let regexp = /\B(?=(\d{3})+(?!\d))/g;
+  let strArray = string.split(`.`);
+  strArray[0] = strArray[0].replace(regexp, `,`);
+  output.textContent = strArray.join(`.`);
+}
+
+// Regular Expression Explanation:
+//
+// From Elias Zamaria @ StackOverflow: https://goo.gl/qpfC1r
+//
+// The regex uses 2 lookahead assertions: a positive one to look for
+// any point in the string that has a multiple of 3 digits in a row
+// after it, and a negative assertion to make sure that point only
+// has exactly a multiple of 3 digits. The replacement expression
+// puts a comma there.
+//
+// For example, if you pass it "123456789.01", the positive assertion
+// will match every spot to the left of the 7 (since "789" is a 
+// multiple of 3 digits, "678" is a multiple of 3 digits, "567", etc.).
+// The negative assertion checks that the multiple of 3 digits does
+// not have any digits after it. "789" has a period after it so it is
+// exactly a multiple of 3 digits, so a comma goes there. "678" is a
+// multiple of 3 digits but it has a "9" after it, so those 3 digits
+// are part of a group of 4, and a comma does not go there. Similarly
+// for "567". "456789" is 6 digits, which is a multiple of 3, so a
+// comma goes before that. "345678" is a multiple of 3, but it has a
+// "9" after it, so no comma goes there. And so on. The "\B" keeps
+// the regex from putting a comma at the beginning of the string.
 
 function operate(operator, num1, num2) {
   switch (operator) {
