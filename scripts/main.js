e291032e4302ef04b51529810b923b64424ad060
Edgar Miranda <edgar.j.miranda@me.com>
2018-10-14 01:56:02 -0500

    Restructure functions to add keyboard support with manual hover classes

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const sign = document.querySelector(`.sign`);
 const perc = document.querySelector(`.perc`);
 const numbers = document.querySelectorAll(`.number`);
 const dot = document.querySelector(`.dot`);
-const operations = document.querySelectorAll(`.operation`);
+const operators= document.querySelectorAll(`.operator`);
 const equal = document.querySelector(`.equal`);
 
 let val = `0`;
@@ -17,60 +17,174 @@ let isAfterEq = false;
 
 for (let number of numbers) {
   number.addEventListener(`click`, (event) => {
-    if (isAfterOp || isAfterEq) {
-      val = (number.textContent === `.`) ? `0` : ``;
-      if (isAfterOp) {
-        isChainable = true;
-      }
-      isAfterOp = false;
-      isAfterEq = false;
-    } else if (/^-*0$/.test(val) && number.textContent !== `.`) {
-      val = /-/.test(val) ? `-` : ``;
-    }
-    val += number.textContent;
-    setClear(val);
-    showOutput(val);
-    checkDot(val);
-    setOperationState(`deselectAll`);
-    logAll(event.target.textContent);
+   numberKey(event.target.textContent); 
   });
 }
 
-for (let operation of operations) {
-  operation.addEventListener(`click`, (event) => {
-    if (isChainable) {
-      doEqual();
-    } else {
-      if (!isAfterOp) {
-        num1 = +val;
-      }
-    }
-    op = event.target.textContent;
-    isAfterOp = true;
-    isAfterEq = false;
-    checkDot(val);
-    setOperationState(`selected`);
-    logAll(event.target.textContent);
+for (let operator of operators) {
+  operator.addEventListener(`click`, (event) => {
+    operatorKey(event.target.textContent);
   });
-  operation.addEventListener(`mouseover`, () => {
-    if (operation.classList.value.includes(`selected`)) {
-      operation.classList.add(`hover`);
+  operator.addEventListener(`mouseover`, () => {
+    if (operator.classList.value.includes(`selected`)) {
+      operator.classList.add(`selected-hover`);
     }
   });
-  operation.addEventListener(`mouseout`, () => {
-    operation.classList.remove(`hover`);
+  operator.addEventListener(`mouseout`, () => {
+    operator.classList.remove(`selected-hover`);
   });
 }
 
 equal.addEventListener(`click`, (event) => {
+  equalKey(event.target);
+});
+
+clear.addEventListener(`click`, (event) => {
+  clearKey(event.target.textContent);
+});
+
+sign.addEventListener(`click`, (event) => {
+  signKey(event.target.textContent);
+});
+
+perc.addEventListener(`click`, (event) => {
+  percKey(event.target.textContent);
+});
+
+window.addEventListener(`keydown`, (event) => {
+  if (/[\d\.]/.test(event.key)) {
+    if (event.key === `.` && dot.disabled) {
+      console.log(`The \`.\` is disabled`);
+    } else {
+      doKeyHover(event.key);
+      numberKey(event.key);
+    }
+  } else if (/[-\/*\+=]|Enter\b/.test(event.key)) {
+    if (event.key !== `=` && event.key !== `Enter`) {
+      operatorKey(event.key);
+    } else {
+      doKeyHover(equal.textContent);
+      equalKey(event.key);
+    }
+  } else if (/[cC]\b|Backspace\b/u.test(event.key)) {
+    doKeyHover(clear.textContent);
+    clearKey(event.key);
+  } else if (event.key === `%`) {
+    doKeyHover(perc.textContent);
+    percKey(event.key);
+  }
+});
+
+function doKeyHover(keyToHover) {
+  let keys = [clear, sign, perc, ...numbers, equal];
+  for (let key of keys) {
+    if (key.textContent === keyToHover) {
+      if (/[\d\.]/.test(keyToHover)) {
+        key.classList.add(`number-hover`);
+      } else {
+        key.classList.add(`others-hover`);
+      }
+      window.setTimeout(() => {
+        key.classList.remove(`number-hover`, `others-hover`);
+      }, 200);
+    }
+  }
+}
+
+function numberKey(key) {
+  if (isAfterOp || isAfterEq) {
+    val = (key === `.`) ? `0` : ``;
+    if (isAfterOp) {
+      isChainable = true;
+    }
+    isAfterOp = false;
+    isAfterEq = false;
+  } else if (/^-*0$/.test(val) && key !== `.`) {
+    val = /-/.test(val) ? `-` : ``;
+  }
+  val += key;
+  setClear();
+  showOutput();
+  checkDot();
+  setOperatorState(`deselectAll`);
+  logAll(key);
+}
+
+function operatorKey(key) {
+  if (isChainable) {
+    doEqual();
+  } else {
+    if (!isAfterOp) {
+      num1 = +val;
+    }
+  }
+  op = key;
+  isAfterOp = true;
+  isAfterEq = false;
+  checkDot();
+  setOperatorState(`selected`);
+  logAll(key);
+}
+
+function equalKey(key) {
   doEqual();
   isChainable = false;
   isAfterOp = false;
   isAfterEq = true;
-  checkDot(val);
-  setOperationState(`deselectAll`);
-  logAll(event.target.textContent);
-});
+  checkDot();
+  setOperatorState(`deselectAll`);
+  if (key === `Enter`) {
+    key = `${key} -> ${equal.textContent}`;   
+  }
+  logAll(key);
+}
+
+function clearKey(key) {
+  let keyToLog = `${key} -> ${clear.textContent}`;
+  if (clear.textContent === `AC`) {
+    num1 = 0;
+    op = ``;
+    num2 = 0;
+    isChainable = false;
+    isAfterOp = false;
+    isAfterEq = false;
+    setOperatorState(`deselectAll`);
+  } else {
+    if (isChainable) {
+      isChainable = false;
+      isAfterOp = true;
+      setOperatorState(`selected`);
+    }
+    clear.textContent = `AC`
+  }
+  val = `0`;
+  showOutput();
+  checkDot();
+  logAll(keyToLog);
+}
+
+function signKey(key) {
+  if (isAfterOp) {
+    val = `0`;
+    isAfterOp = false;
+  }
+  if (val.startsWith(`-`)) {
+    val = val.substring(1);
+  } else {
+    val = `-${val}`;
+  }
+  showOutput();
+  logAll(key);
+}
+
+function percKey(key) {
+  if (val !== `0`) {
+    val = (+val / 100).toString();
+  }
+  showOutput();
+  checkDot();
+  logAll(key);
+}
 
 function doEqual() {
   if (op !== ``) {
@@ -86,88 +200,49 @@ function doEqual() {
     }
     num1 = +val;
   }
-  showOutput(val);
+  showOutput();
 }
 
-clear.addEventListener(`click`, (event) => {
-  let keyToLog = event.target.textContent;
-  if (clear.textContent === `AC`) {
-    num1 = 0;
-    op = ``;
-    num2 = 0;
-    isChainable = false;
-    isAfterOp = false;
-    isAfterEq = false;
-    setOperationState(`deselectAll`);
-  } else {
-    if (isChainable) {
-      isChainable = false;
-      isAfterOp = true;
-      setOperationState(`selected`);
-    }
-    clear.textContent = `AC`
+function operate(op, num1, num2) {
+  switch (op) {
+    case `+`:
+      return add(num1, num2);
+    case `−`:
+      return substract(num1, num2);
+    case `×`:
+      return multiply(num1, num2);
+    case `÷`:
+      return divide(num1, num2);
   }
-  val = `0`;
-  showOutput(val);
-  checkDot(val);
-  logAll(keyToLog);
-});
+}
 
-sign.addEventListener(`click`, (event) => {
-  if (isAfterOp) {
-    val = `0`;
-    isAfterOp = false;
-  }
-  if (val.startsWith(`-`)) {
-    val = val.substring(1);
-  } else {
-    val = `-${val}`;
-  }
-  showOutput(val);
-  logAll(event.target.textContent);
-});
+function add(num1, num2) {
+  return num1 + num2;
+}
 
-perc.addEventListener(`click`, (event) => {
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 !== 0) {
+    return num1 / num2;  
+  }
+  return `Error`;
+}
+
+function setClear() {
   if (val !== `0`) {
-    val = (+val / 100).toString();
-  }
-  showOutput(val);
-  logAll(event.target.textContent);
-});
-
-function setClear(string) {
-  if (string !== `0`) {
     clear.textContent = `C`;
   }
 }
 
-function setOperationState(action) {
-  switch (action) {
-    case `deselectAll`:
-      for (let operation of operations) {
-        operation.classList.remove(`selected`);
-      }   
-      break;
-    case `selected`:
-      setOperationState(`deselectAll`);
-      for (let operation of operations) {
-        if (operation.textContent === op) {
-          operation.classList.add(`selected`);
-        }
-      }   
-      break;
-  }
-}
-
-function checkDot(string) {
-  dot.disabled = false;
-  if (/\./.test(string) && !isAfterOp && !isAfterEq) {
-    dot.disabled = true;
-  }
-}
-
-function showOutput(string) {
-  output.textContent = formatForScr(string);
+function showOutput() {
+  output.textContent = formatForScr(val);
 }
 
 function formatForScr(string) {
@@ -175,23 +250,27 @@ function formatForScr(string) {
     
     // The longest string to display will have at most:
     // 9 numbers
-    // 2 commas || 1 comma && 1 dot || 1 dot
+    // 2 commas || 2 commas && 1 dot || 1 comma && 1 dot || 1 dot
     // - sign || no sign
     // 
     // Case 1: 2 commas -> Max length = 11 w/o sign & 12 w/ sign
-    // Sample:  - 3 3 3 , 3 3 3 , 3  3  3
+    // Sample:  - 3 3 3 , 3 3 3 , 3  3  3 <- Only 9 numbers allowed 
     // Digits:  1 2 3 4 5 6 7 8 9 10 11 12
     //
-    // Case 2: 1 comma && 1 dot -> Max length = 11 w/o sign & 12 w/ sign
-    // Sample:  - 3 3 , 3 3 3 . 3 3  3  3
+    // Case 2: 2 commas && 1 dot -> Max length = 12 w/o sign & 13 w/ sign
+    // Sample:  - 3 , 3 3 3 , 3 3 3  .  3  3 <- Only 9 numbers allowed
+    // Digits:  1 2 3 4 5 6 7 8 9 10 11 12 13
+    //
+    // Case 3: 1 comma && 1 dot -> Max length = 11 w/o sign & 12 w/ sign
+    // Sample:  - 3 3 , 3 3 3 . 3 3  3  3 <- Only 9 numbers allowed
     // Digits:  1 2 3 4 5 6 7 8 9 10 11 12
     //
-    // Case 3: 1 dot -> Max length = 10 w/o sign & 11 w/ sign
-    // Sample:  - 3 . 3 3 3 3 3 3 3  3 <- Only 9 numbers allowed 
+    // Case 4: 1 dot -> Max length = 10 w/o sign & 11 w/ sign
+    // Sample:  - 3 . 3 3 3 3 3 3 3  3 <- Only 9 numbers allowed
     // Digits:  1 2 3 4 5 6 7 8 9 10 11
     //
     // SPECIAL CASE
-    // Case 4: Exponential Syntax -> Max length = 10 w/o sign & 11 w/ sign
+    // Case 5: Exponential Syntax -> Max length = 10 w/o sign & 11 w/ sign
     // Sample:  - 3 . 3 3 3 3 e + 3  3 <- doEqual() sets 4 #s after the dot
     // Digits:  1 2 3 4 5 6 7 8 9 10 11
     //
@@ -236,13 +315,13 @@ function formatForScr(string) {
     // the regex from putting a comma at the beginning of the string.
 
     // 3. Set the font size to accomodate all digits on the screen
-    if (string.length > 6) {
-      let canvas = document.createElement(`canvas`);
-      let context = canvas.getContext(`2d`);
-      context.font = `80px 'SF-Pro-Display-Light'`;
-      let metrics = context.measureText(string);
+    let canvas = document.createElement(`canvas`);
+    let context = canvas.getContext(`2d`);
+    context.font = `80px 'SF-Pro-Display-Light'`;
+    let metrics = context.measureText(string);
+    if (metrics.width > 284) { // The output is set to 284px;
       let widthToHeightRatio = metrics.width / 80;
-      let size = 284 / widthToHeightRatio; // The output is set to 284px;
+      let size = 284 / widthToHeightRatio;
       output.style.fontSize = `${size}px`;
     } else {
       output.style.fontSize = `80px`;
@@ -254,6 +333,31 @@ function formatForScr(string) {
   }
 }
 
+function checkDot() {
+  dot.disabled = false;
+  if (/\./.test(val) && !isAfterOp && !isAfterEq) {
+    dot.disabled = true;
+  }
+}
+
+function setOperatorState(action) {
+  switch (action) {
+    case `deselectAll`:
+      for (let operator of operators) {
+        operator.classList.remove(`selected`);
+      }   
+      break;
+    case `selected`:
+      setOperatorState(`deselectAll`);
+      for (let operator of operators) {
+        if (operator.textContent === op) {
+          operator.classList.add(`selected`);
+        }
+      }   
+      break;
+  }
+}
+
 function logAll(key) {
   console.log(`Key Pressed = \`${key}\``);
   console.log(`Screen Value = \`${formatForScr(val)}\``);
@@ -261,36 +365,4 @@ function logAll(key) {
   console.log(`isAfterOp = ${isAfterOp}, isAfterEq = ${isAfterEq}, isChainable = ${isChainable}`);
   console.log(`The \`.\` is ${(dot.disabled) ? `disabled` : `enabled`}`);
   console.log(``);
-}
-
-function operate(op, num1, num2) {
-  switch (op) {
-    case `+`:
-      return add(num1, num2);
-    case `−`:
-      return substract(num1, num2);
-    case `×`:
-      return multiply(num1, num2);
-    case `÷`:
-      return divide(num1, num2);
-  }
-}
-
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-function substract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  if (num2 !== 0) {
-    return num1 / num2;  
-  }
-  return `Error`;
 }
